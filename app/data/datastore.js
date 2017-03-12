697fe00b808907249148e646c060230c8188f0e7
Don Knapp <don@dknapp.com>
2017-03-11 19:32:49 -0500

    Reset the data store

--- app/data/datastore.js
+++ app/data/datastore.js
@@ -1,1 +1,5 @@
-[{"name":"Jack Sprat","photo":"http://vignette2.wikia.nocookie.net/pussinbootsnetflix/images/d/da/EP_3.jpg/revision/latest?cb=20160209095157","scores":["3","3","3","3","3","3","3","3","3","3"]},{"name":"x","photo":"y","scores":["3","3","3","3","3","3","3","3","3","3"]},{"name":"x","photo":"y","scores":["3","3","3","3","3","3","3","3","3","3"]},{"name":"x","photo":"x","scores":["3","3","3","3","3","3","3","3","3","3"]},{"name":"x","photo":"y","scores":["3","3","3","3","3","3","3","3","3","3"]}]
+[{
+    "name": "Jack Sprat",
+    "photo": "http://vignette2.wikia.nocookie.net/pussinbootsnetflix/images/d/da/EP_3.jpg/revision/latest?cb=20160209095157",
+    "scores": ["3", "3", "3", "3", "3", "3", "3", "3", "3", "3"]
+}]
